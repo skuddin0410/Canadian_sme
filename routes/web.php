678631a6819396e646ae2e7b6d3005d472afae84
SubhabrataKayal <subhabrata@digitalaptech.co.in>
2025-08-05 13:02:02 +0530

    Reformat router

--- routes/web.php
+++ routes/web.php
@@ -10,20 +10,10 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::group(['middleware' => 'auth:web'], function () {
-    Route::get('account-information', [App\Http\Controllers\HomeController::class, 'accountInfo'])->name('change.account.information');
-    Route::post('account-information', [App\Http\Controllers\HomeController::class, 'accountInformation'])->name('change.account.information.post');
-
-    Route::get('change-password', [App\Http\Controllers\HomeController::class, 'changeAccountPassword'])->name('admin.change.password');
-    Route::post('change-password', [App\Http\Controllers\HomeController::class, 'changePassword'])->name('admin.change.password.post');
-
-    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-   require __DIR__.'/admin.php';
+   require __DIR__.'/common.php'; //used by all users in web
+   require __DIR__.'/admin.php';  //Admin and Event Admin 
    require __DIR__.'/exhibitor.php';
    require __DIR__.'/speaker.php';
    require __DIR__.'/attendee.php';
    require __DIR__.'/helpdesk.php';
-
-   
-
 });
