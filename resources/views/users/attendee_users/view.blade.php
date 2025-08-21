4d484a2e9e0db3badaec4a4686741f6c41a38b34
SubhabrataKayal <subhabrata@digitalaptech.co.in>
2025-08-21 16:33:47 +0530

    fixed

--- resources/views/users/attendee_users/view.blade.php
+++ resources/views/users/attendee_users/view.blade.php
@@ -15,20 +15,11 @@ Admin | Attendee Details
     <div class="row">
         <div class="col-xl">
             <div class="card mb-4">
-                <div class="card-header d-flex justify-content-between align-items-center">
-                    <div class="dt-action-buttons text-end pt-3 pt-md-0">
-                        <div class="dt-buttons"></div>
-                    </div>
-                </div>
-
                 <div class="card-body">
-                    <div class="d-flex pt-3 justify-content-end">
-                        <a href="{{ route('attendee-users.index') }}"
-                            class="btn btn-outline-primary btn-pill btn-streach font-book ml-3 mt-6 fs-14">Back</a>
-                    </div>
-                    <div class="d-flex pt-3 justify-content-end">
-                      
-                        <form action="{{ route('users.toggleBlock', $user->id) }}" method="POST">
+
+                    <div class="d-flex justify-content-end">
+                        
+                        <form action="{{ route('users.toggleBlock', $user->id) }}" method="POST" class="d-inline">
                             @csrf
                             @method('PATCH')
                             <button type="submit" class="btn btn-danger btn-pill btn-streach font-book fs-14 me-2">
@@ -37,22 +28,30 @@ Admin | Attendee Details
                         </form>
           
                         
-                        <form action="{{ route('helpdesk.users.unblock', $user->id) }}" method="POST">
+                        <form action="{{ route('helpdesk.users.unblock', $user->id) }}" method="POST" class="d-inline">
                             @csrf
                             @method('PATCH')
-                            <button type="submit" class="btn btn-warning btn-pill btn-streach font-book fs-14">
+                            <button type="submit" class="btn btn-warning btn-pill btn-streach font-book fs-14 me-2">
                                 Unblock User
+                            </button>
+                        </form>
+
+                        <form action="{{ route('attendee-users.index') }}" method="GET" class="d-inline">
+                            @csrf
+                            
+                            <button type="submit" class="btn btn-outline-primary btn-pill btn-streach font-book fs-14 me-2">
+                               <i class="fa fa-angle-left me-1"></i> Back
                             </button>
                         </form>
                       
                     </div>
 
-                    <h5 class="pb-2 border-bottom mb-4">Attendee Details</h5>
+                    <h5 class="border-bottom mb-4">Attendee Details: {{ $user->full_name }}</h5>
 
                     <div class="info-container">
                         <div class="row">
                             <!-- Left Column -->
-                            <div class="col-12">
+                            <div class="col-6">
                                 <ul class="list-unstyled justify-content-between">
                                     <li class="mb-3"><span class="fw-medium me-2">Name:</span> <span>{{ $user->name }}
                                             {{ $user->lastname }}</span></li>
