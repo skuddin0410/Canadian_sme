4494b072a7ea2adbcaed7e3b6a37fc37be0d653a
SubhabrataKayal <subhabrata@digitalaptech.co.in>
2025-08-22 12:21:38 +0530

    bio

--- resources/views/users/admin_users/view.blade.php
+++ resources/views/users/admin_users/view.blade.php
@@ -1,63 +1,99 @@
 @extends('layouts.admin')
 
 @section('title')
-Admin |  Admin user Details
+Admin |  Team User Details
 @endsection
 @section('content')
 <div class="container-xxl flex-grow-1 container-p-y pt-0">
-    <h4 class="py-3 mb-4"><span class="text-muted fw-light"></span> Admin user</h4>
+    <h4 class="py-3 mb-4"><span class="text-muted fw-light"></span>Team</h4>
+
     <div class="row">
         <div class="col-xl">
             <div class="card mb-4">
-              <div class="card-header d-flex justify-content-between align-items-center">
-                <div class="dt-action-buttons text-end pt-3 pt-md-0">
-                  <div class="dt-buttons"> 
-                  </div>
-                </div>
-              </div>
-              <div class="card-body">
-                <div class="d-flex pt-3 justify-content-end">
-                      <a href="{{route("admin-users.edit",["admin_user"=> $user->id ])}}" class="btn btn-outline-primary btn-pill btn-streach font-book me-2 mt-6 fs-14 ">Edit</a>
-                      <a href="{{route("admin-users.index")}}" class="btn btn-outline-primary btn-pill btn-streach font-book ml-3 mt-6 fs-14">Back</a>
-                </div>
-                <h5 class="pb-2 border-bottom mb-4"> Admin user Details</h5>
-                <div class="info-container">
-                <div class="row">  
-                    <div class="col-6"> 
-                        <ul class="list-unstyled justify-content-between">
-                          <li class="mb-3">
-                            <span class="fw-medium me-2">Name:</span>
-                            <span>{{ $user->name }} {{ $user->lastname }}</span>
-                          </li>
-                           <li class="mb-3">
-                            <span class="fw-medium me-2">User Name:</span>
-                            <span>{{ $user->username ?? ''}}</span>
-                          </li>
-                          <li class="mb-3">
-                            <span class="fw-medium me-2">Email:</span>
-                            <span>{{ $user->email }}</span>
-                          </li>
-                          <li class="mb-3">
-                            <span class="fw-medium me-2">Mobile:</span>
-                            <span>{{ $user->mobile ?? '' }}</span>
-                          </li>
-                          <li class="mb-3">
-                            <span class="fw-medium me-2">Referral Coupon:</span>
-                            <span>{{ $user->referral_coupon ?? '' }}</span>
-                          </li>
-              
-                        </ul>
+                <div class="card-body">
+
+                    <div class="d-flex justify-content-end">
+                        
+                        <form action="{{ route('users.toggleBlock', $user->id) }}" method="POST" class="d-inline">
+                            @csrf
+                            @method('PATCH')
+                            <button type="submit" class="btn btn-danger btn-pill btn-streach font-book fs-14 me-2">
+                                Block User
+                            </button>
+                        </form>
+          
+                        
+                        <form action="{{ route('helpdesk.users.unblock', $user->id) }}" method="POST" class="d-inline">
+                            @csrf
+                            @method('PATCH')
+                            <button type="submit" class="btn btn-warning btn-pill btn-streach font-book fs-14 me-2">
+                                Unblock User
+                            </button>
+                        </form>
+
+                        <form action="{{ route('sponsors.index') }}" method="GET" class="d-inline">
+                            <button type="submit" class="btn btn-outline-primary btn-pill btn-streach font-book fs-14 me-2">
+                               <i class="fa fa-angle-left me-1"></i> Back
+                            </button>
+                        </form>
+                      
                     </div>
-                
-                 <div class="d-flex pt-3 justify-content-end">
-      
-                      {{-- <a href="{{route("admin-users.edit",["admin_user"=> $user->id ])}}" class="btn btn-outline-primary btn-pill btn-streach font-book me-2 mt-6 fs-14 ">Edit</a>
-                      <a href="{{route("admin-users.index")}}" class="btn btn-outline-primary btn-pill btn-streach font-book ml-3 mt-6 fs-14">Back</a> --}}
+
+                    <h5 class="border-bottom mb-4">Sponsors Details: {{ $user->full_name }}</h5>
+
+                    <div class="info-container">
+                    <div class="row">   
+                   <div class="text-left mb-2">
+                    
+                    <label for="profileImageInput">
+                      <img id="profileImagePreview" 
+                           src="{{!empty($user->photo) ? $user->photo->file_path : ''}}" 
+                           class="rounded-circle border border-2" 
+                           style="width: 150px; height: 150px; object-fit: cover; cursor: pointer;">
+                    </label>
+                    
+                  </div>     <div class="col-6">
+                                <ul class="list-unstyled justify-content-between">
+                                    <li class="mb-3"><span class="fw-medium me-2">Name:</span> <span>{{ $user->name }}
+                                            {{ $user->lastname }}</span></li>
+                                   
+                                    <li class="mb-3"><span class="fw-medium me-2">Email:</span>
+                                        <span>{{ $user->email }}</span></li>
+                                    <li class="mb-3"><span class="fw-medium me-2">Designation:</span>
+                                        <span>{{ $user->designation ?? '-' }}</span></li>
+                                    <li class="mb-3"><span class="fw-medium me-2">Mobile:</span>
+                                        <span>{{ $user->mobile ?? '' }}</span></li>
+
+                                    <li class="mb-3"><span class="fw-medium me-2">Tags:</span>
+                                        @if(!empty($user->tags))
+                                        @foreach(explode(',', $user->tags) as $tag)
+                                        <span class="badge bg-primary me-1">{{ trim($tag) }}</span>
+                                        @endforeach
+                                        @else
+                                        <span>-</span>
+                                        @endif
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="col-6">
+                                <ul class="list-unstyled justify-content-between">
+                                    <li class="mb-3"><span class="fw-medium me-2">Website:</span> <span>{{ $user->website_url }}</span></li>
+                                   
+                                   <li class="mb-3"><span class="fw-medium me-2">Linkedin:</span> <span>{{ $user->linkedin_url }}</span></li>
+
+                                   <li class="mb-3"><span class="fw-medium me-2">Facebook:</span> <span>{{ $user->facebook_url }}</span></li>
+                                   <li class="mb-3"><span class="fw-medium me-2">Instagram:</span> <span>{{ $user->instagram_url }}</span></li>
+                                   <li class="mb-3"><span class="fw-medium me-2">Twitter:</span> <span>{{ $user->twitter_url }}</span></li> 
+                                </ul>
+                            </div>
+
+                        </div>
+                    </div>
+
                 </div>
-                </div>
-              </div>
-          </div>
-		</div>
+            </div>
+
+        </div>
     </div>
 </div>
 @endsection
