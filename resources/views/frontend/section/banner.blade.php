0ef5acb46247e1cb1017b097a242d2db2a64dccf
subhamita <subhamita.chakraborty@digitalaptech.co.in>
2025-09-12 13:34:24 +0530

    added youtube url in the banner

--- resources/views/frontend/section/banner.blade.php
+++ resources/views/frontend/section/banner.blade.php
@@ -1,19 +1,23 @@
 <!-- banner -->  
 @if(!empty($event) && !empty($event->photo))
     <section class="banner position-relative" style="background-image: url({{$event->photo->file_path}})">
-        <div class="conatainer">
-            <button class="position-absolute top-50 start-50 translate-middle rounded-circle overflow-hidden">
-                <img class="banner-play-btn" src="{{asset('frontend/images/video-circle.png')}}" alt="">
-            </button>
+        <div class="container text-center">
+            @if(!empty($event->youtube_link))
+                <a href="{{ $event->youtube_link }}" target="_blank" class="position-absolute top-50 start-50 translate-middle rounded-circle overflow-hidden">
+                    <img class="banner-play-btn" src="{{ asset('frontend/images/video-circle.png') }}" alt="Play Video">
+                </a>
+            @endif
         </div>
     </section>
- @else
-    <section class="banner position-relative" style="background-image: url({{asset('frontend/images/banner.png')}})">
-        <div class="conatainer">
-            <button class="position-absolute top-50 start-50 translate-middle rounded-circle overflow-hidden">
-                <img class="banner-play-btn" src="{{asset('frontend/images/video-circle.png')}}" alt="">
-            </button>
+@else
+    <section class="banner position-relative" style="background-image: url({{ asset('frontend/images/banner.png') }})">
+        <div class="container text-center">
+            @if(!empty($event->youtube_link))
+                <a href="{{ $event->youtube_link }}" target="_blank" class="position-absolute top-50 start-50 translate-middle rounded-circle overflow-hidden">
+                    <img class="banner-play-btn" src="{{ asset('frontend/images/video-circle.png') }}" alt="Play Video">
+                </a>
+            @endif
         </div>
     </section>
- @endif
-    <!-- banner end -->
+@endif
+<!-- banner end -->
