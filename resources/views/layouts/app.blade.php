a753828238ff450fdd3061fdc3714f4b4c3657e6
SubhabrataKayal <subhabrata@digitalaptech.co.in>
2025-09-20 03:10:55 +0530

    fixed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand logo" href="{{ url('/') }}">
-                    <img src="{{asset('sme-logo.png')}}" alt="{{ config('app.name', 'SME') }}" width="20%">
+                    <img src="{{asset('sme-logo.png')}}" alt="{{ config('app.name', 'SME') }}" width="70%">
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
