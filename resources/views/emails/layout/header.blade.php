21f32b26dc6868b0e03591e3a2c34336370319c2
SubhabrataKayal <subhabrata@digitalaptech.co.in>
2025-09-22 14:26:47 +0530

    fixed

--- resources/views/emails/layout/header.blade.php
+++ resources/views/emails/layout/header.blade.php
@@ -48,11 +48,8 @@
           <!-- Header -->
           <tr>
            <td align="left" bgcolor="#002364" style="padding:20px; display: flex; align-items: center; color: white;">
-    <img src="{{asset('images/footer-logo.png')}}" alt="Company Logo" width="180" height="40"
-  style="
-    display:block;border:0;outline:none;text-decoration:none;-ms-interpolation-mode:bicubic;
-    width:180px !important;height:auto !important;max-width:100%;margin-right:15px;
-  ">
+    <img src="{{asset('images/footer-logo.png')}}" alt="Company Logo" width="180" height="61"
+  style="display:block;border:0;outline:none;text-decoration:none;-ms-interpolation-mode:bicubic;width:180px !important;height:61px !important;max-width:100%;margin-right:15px;margin-top:23px;">
     <div style="color:white;text-decoration: none;">
       <p style="font-size: 15px;">{{ getKeyValue('company_name')->value }}</p>
       <p style="font-size: 15px;">{{ getKeyValue('company_address')->value }}</p>
