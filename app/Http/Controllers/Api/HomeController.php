7e8728c760d326359b318ef0570829f2b22217a1
subhabratakayal0605 <subhabrata@digitalaptech.co.in>
2025-10-13 16:19:51 +0530

    fixed

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -73,7 +73,7 @@ class HomeController extends Controller
                     "panels" => $session->panels ?? '',
                     "start_time" => $session->start_time ?? '',
                     "end_time" => $session->end_time ?? '',
-                    "workshop_no" => "Workshop NO : " . str_pad($session->id, 2, '0', STR_PAD_LEFT),
+                    "workshop_no" => $session->track ?? '',
                     "location" => !empty($session->location) ? $session->location: '',
                     "status" => $session->status ?? 'Upcoming',
                     "speakers" => $session->speakers->map(fn ($sp) => ["name" => $sp->name, "image"=> !empty($sp->photo) ? $sp->photo->file_path : asset('images/default.png')]),
