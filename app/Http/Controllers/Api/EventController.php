d34897210294da92a5a3496642e6a114c8ed4f0c
joydeepdapl <joydeep.kundu@digitalaptech.co.in>
2026-02-05 16:42:50 +0530

    working on get all events api

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -6,12 +6,36 @@ use App\Http\Requests\EventRequest;
 use App\Http\Resources\EventResource;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Carbon\Carbon;
 
 class EventController extends Controller
 {
-    public function index()
+    // public function index()
+    // {
+    //     return EventResource::collection(Event::latest()->paginate(10));
+    // }
+
+    public function index(Request $request)
     {
-        return EventResource::collection(Event::latest()->paginate(10));
+        $type = $request->type; // past | ongoing | future
+        $today = Carbon::today();
+
+        $query = Event::query();
+
+        if ($type === 'past') {
+            $query->whereDate('end_date', '<', $today);
+        }
+
+        if ($type === 'ongoing') {
+            $query->whereDate('start_date', '<=', $today)
+                ->whereDate('end_date', '>=', $today);
+        }
+
+        if ($type === 'future') {
+            $query->whereDate('start_date', '>', $today);
+        }
+
+        return EventResource::collection($query->latest()->paginate(10));
     }
 
     public function store(EventRequest $request)
