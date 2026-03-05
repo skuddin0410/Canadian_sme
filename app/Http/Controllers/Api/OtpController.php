ab302d6ec702d40793cb7d2dc8fa24bdb6b40dc8
joydeepdapl <joydeep.kundu@digitalaptech.co.in>
2026-03-05 12:38:43 +0530

    feat: Refactor user verification process in OtpController with enhanced error handling

--- app/Http/Controllers/Api/OtpController.php
+++ app/Http/Controllers/Api/OtpController.php
@@ -119,129 +119,129 @@ class OtpController extends Controller
         
     }
 
-public function verify(Request $request)
-{  
-        $validator = Validator::make($request->all(), [
-            'email' => 'required|string|email|max:255',
-            'otp'   => 'required|digits:4',
-        ]);
-        
-        if (User::where('email', $request->email)->doesntExist()) {
-            return response()->json([
-               'success' => false,
-               'message' => 'You are not approved by admin.',
-            ],403);  
-        }
-             
-        if (User::onlyTrashed()->where('email', $request->email)->first()) {
-            return response()->json([
+    public function verify(Request $request)
+    {  
+            $validator = Validator::make($request->all(), [
+                'email' => 'required|string|email|max:255',
+                'otp'   => 'required|digits:4',
+            ]);
+            
+            if (User::where('email', $request->email)->doesntExist()) {
+                return response()->json([
                 'success' => false,
-                'message' => 'Your account is deleted or block.',
-            ], 403); 
-        }
-
-        if (User::where('email', $request->email)->where('is_approve', 0)->exists()) {
-            return response()->json([
-                'success' => false,
-                'message' => 'Your account is inactive.',
-            ], 403); 
-        }
-    
-        $allowedEmails = [
-            "henry.roy@example.com",
-            "subhabrata1@example.com"
-        ];
-
-        
-
-    if ($validator->fails()) {
-        return response()->json([
-            'success' => false,
-            'message' => $validator->errors(),
-        ], 422);
-    }
-
-       $otp = null;
-        if (!in_array($request->email, $allowedEmails)) {
-            $otp = Otp::where('email', $request->email)
-                ->where('otp', $request->otp)
-                ->where('expired_at', '>', Carbon::now())
-                ->first();
-           
-            if (!$otp) {
+                'message' => 'You are not approved by admin.',
+                ],403);  
+            }
+                
+            if (User::onlyTrashed()->where('email', $request->email)->first()) {
                 return response()->json([
                     'success' => false,
-                    'message' => 'Invalid or expired OTP',
-                ], 400);
+                    'message' => 'Your account is deleted or block.',
+                ], 403); 
             }
-       }
-   
-    $user = User::firstOrCreate(
-        ['email' => $request->email],
-        ['password' => Hash::make($request->otp)]
-    );
-    
-    $user->assignRole('Attendee');
-    
-    try {
 
-        if($user->is_approve == 0){
-           return response()->json([
-            'success'    => false,
-            'message'    => 'Your account is inactive.',
-           ]); 
-        }
+            if (User::where('email', $request->email)->where('is_approve', 0)->exists()) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Your account is inactive.',
+                ], 403); 
+            }
         
-        $credentials = [
-            'email'    => $request->email,
-            'password' => $request->otp, 
-        ];
+            $allowedEmails = [
+                "henry.roy@example.com",
+                "subhabrata1@example.com"
+            ];
 
-        $token = JWTAuth::fromUser($user);
-      
-        if (! $token ) {
+            
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => $validator->errors(),
+            ], 422);
+        }
+
+        $otp = null;
+            if (!in_array($request->email, $allowedEmails)) {
+                $otp = Otp::where('email', $request->email)
+                    ->where('otp', $request->otp)
+                    ->where('expired_at', '>', Carbon::now())
+                    ->first();
+            
+                if (!$otp) {
+                    return response()->json([
+                        'success' => false,
+                        'message' => 'Invalid or expired OTP',
+                    ], 400);
+                }
+        }
+    
+        $user = User::firstOrCreate(
+            ['email' => $request->email],
+            ['password' => Hash::make($request->otp)]
+        );
+        
+        $user->assignRole('Attendee');
+        
+        try {
+
+            if($user->is_approve == 0){
+            return response()->json([
+                'success'    => false,
+                'message'    => 'Your account is inactive.',
+            ]); 
+            }
+            
+            $credentials = [
+                'email'    => $request->email,
+                'password' => $request->otp, 
+            ];
+
+            $token = JWTAuth::fromUser($user);
+        
+            if (! $token ) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Invalid OTP.',
+                ], 401);
+            }
+            
+            $user->update([
+            'jwt_token' => $token
+            ]);
+    
+            $session = SessionDate::updateOrCreate(
+                ['user_id' => $user->id], 
+                ['expires_at' => now()->addMonths(2)] 
+            );
+            
+            notification($user->id);
+            $user = User::where('id',$user->id)->first();
+            if(empty($user->qr_code)){
+                $user->refresh();
+                $qrGenerated = qrCode($user->id);
+                if (!empty($user->qr_code) && $qrGenerated) {
+                    sendNotification("Welcome Email", $user);
+                }
+            }
+            if ($otp) {
+            $otp->delete();
+            }
+            return response()->json([
+                'success'    => true,
+                'message'    => 'Login successful',
+                'token'      => $token,
+                'expires_at' => $session->expires_at,
+            ]);
+
+        } catch (JWTException $e) {
             return response()->json([
                 'success' => false,
                 'message' => 'Invalid OTP.',
-            ], 401);
+                'error'   => $e->getMessage(),
+            ], 500);
         }
-        
-        $user->update([
-          'jwt_token' => $token
-        ]);
-  
-        $session = SessionDate::updateOrCreate(
-            ['user_id' => $user->id], 
-            ['expires_at' => now()->addMonths(2)] 
-        );
-        
-        notification($user->id);
-        $user = User::where('id',$user->id)->first();
-        if(empty($user->qr_code)){
-            $user->refresh();
-            $qrGenerated = qrCode($user->id);
-            if (!empty($user->qr_code) && $qrGenerated) {
-                sendNotification("Welcome Email", $user);
-            }
-        }
-        if ($otp) {
-          $otp->delete();
-        }
-        return response()->json([
-            'success'    => true,
-            'message'    => 'Login successful',
-            'token'      => $token,
-            'expires_at' => $session->expires_at,
-        ]);
-
-    } catch (JWTException $e) {
-        return response()->json([
-            'success' => false,
-            'message' => 'Invalid OTP.',
-            'error'   => $e->getMessage(),
-        ], 500);
     }
-}
 
 
 }
