8ebe639ea51ca17b60877fe4c6fdc839a0027b2e
joydeepdapl <joydeep.kundu@digitalaptech.co.in>
2026-03-09 16:30:23 +0530

    feat: Add favicon link to the layout for improved branding

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+    <link rel="icon" href="{{asset('eventzen-fav-icon.png')}}" type="image/x-icon" />
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
