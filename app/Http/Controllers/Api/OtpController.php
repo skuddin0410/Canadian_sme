217acda63f0362bd1989d8dfa5c35b47098d799f
joydeepdapl <joydeep.kundu@digitalaptech.co.in>
2026-03-10 16:47:21 +0530

    feat: Log complete OTP verification attempt details for improved tracking

--- app/Http/Controllers/Api/OtpController.php
+++ app/Http/Controllers/Api/OtpController.php
@@ -121,7 +121,8 @@ class OtpController extends Controller
 
     public function verify(Request $request)
     {  
-        log::info('OTP Verification Attempt: ' . $request->email);
+        log::info('OTP Verification Attempt: ' . $request->all());
+
             $validator = Validator::make($request->all(), [
                 'email' => 'required|string|email|max:255',
                 'otp'   => 'required|digits:4',
