fe456a0c7b659ea2f18cbf45f64bde512e958a2f
PriyoDaplHalder <priyadarsi.halder@digitalaptech.co.in>
2026-05-11 17:16:37 +0530

    refactor: update badge PDF generation to always stream the file and comment out Bootstrap JS import in the view

--- resources/views/DragAndDropBadge/index.blade.php
+++ resources/views/DragAndDropBadge/index.blade.php
@@ -151,7 +151,7 @@
 </div>
 
 <!-- Bootstrap 5 JS -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script> -->
 
 <script>
     document.getElementById('labelType').addEventListener('change', function () {
