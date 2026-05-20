499ad84f48bbf426b07621d4e702640dadd56e2c
joydeepdapl <joydeep.kundu@digitalaptech.co.in>
2026-05-20 15:05:22 +0530

    fixing

--- resources/views/events/edit.blade.php
+++ resources/views/events/edit.blade.php
@@ -335,6 +335,44 @@
               @enderror
             </div>
 
+            @php
+              $defaultTimezone = old('timezone', $e->timezone ?? config('app.timezone'));
+              $preferredTimezones = [
+                'America/Toronto',
+                'America/New_York',
+                'America/Chicago',
+                'America/Denver',
+                'America/Los_Angeles',
+                'America/Vancouver',
+                'Europe/London',
+                'Europe/Paris',
+                'Asia/Dubai',
+                'Asia/Kolkata',
+                'Asia/Singapore',
+                'Australia/Sydney',
+                'UTC',
+              ];
+              $timezoneOptions = array_values(array_unique(array_merge($preferredTimezones, \DateTimeZone::listIdentifiers())));
+            @endphp
+
+            <div class="mb-3">
+              <label class="form-label" for="timezone">Time Zone <span class="text-danger">*</span></label>
+              <div class="input-group input-group-merge">
+                <span class="input-group-text"><i class="bx bx-world"></i></span>
+                <select
+                  class="form-select @error('timezone') is-invalid @enderror"
+                  name="timezone"
+                  id="timezone">
+                  @foreach($timezoneOptions as $timezone)
+                    <option value="{{ $timezone }}" @selected($defaultTimezone === $timezone)>{{ $timezone }}</option>
+                  @endforeach
+                </select>
+              </div>
+              @error('timezone')
+                <div class="invalid-feedback d-block">{{ $message }}</div>
+              @enderror
+            </div>
+
             {{-- YouTube link --}}
             <div class="mb-3">
               <label class="form-label" for="youtube_link">YouTube Link</label>
